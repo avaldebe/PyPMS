9a3f5af5ef0f1381aca4929fd5cab63299a1538f
Alvaro Valdebenito <alvarov@met.no>
2019-08-19 10:13:43 +0200

    add number count

--- pypms.py
+++ pypms.py
@@ -12,9 +12,18 @@ from serial import Serial
 
 @dataclass
 class Obs:
+    # pmX [ug/m3]: PM1.0, PM2.5 & PM10
     pm01: int
     pm25: int
     pm10: int
+    # nX_Y [#/100cc]: number concentrations under X.Y um
+    n0_3: int
+    n0_5: int
+    n1_0: int
+    n2_5: int
+    n5_0: int
+    n10_0: int
+    # seconds since epoch
     time: int = int(time.time())
 
     def timestamp(self):
@@ -23,8 +32,11 @@ class Obs:
     def str_pm(self):
         return f"PM1 {self.pm01}, PM2.5 {self.pm25}, PM10 {self.pm10} ug/m3"
 
+    def str_nc(self):
+        return f"N0.3 {self.n0_3}, N0.5 {self.n0_5}, N1.0 {self.n1_0}, N2.5 {self.n2_5}, N5.0 {self.n5_0}, N10 {self.n10_0} #/100cc"
+
     def csv(self):
-        return f"{self.time}, {self.pm01}, {self.pm25}, {self.pm10}"
+        return f"{self.time}, {self.pm01}, {self.pm25}, {self.pm10}, {self.n0_3}, {self.n0_5}, {self.n1_0}, {self.n2_5}, {self.n5_0}, {self.n10_0}"
 
     def __str__(self):
         return f"{self.timestamp()}: {self.str_pm()}"
@@ -42,7 +54,7 @@ def decode(buffer: List[int]) -> Obs:
     checksum = sum(buffer[:-2])
     assert msg[-1] == checksum, f"checksum {msg[-1]:#x} != {checksum:#x}"
 
-    return Obs(*msg[5:8])
+    return Obs(*msg[5:14])
 
 
 def read(device: str = "/dev/ttyUSB0") -> Obs:
@@ -58,7 +70,6 @@ def read(device: str = "/dev/ttyUSB0") -> Obs:
     dev.flush()
     while dev.in_waiting < 32:
         continue
-
     return decode(dev.read(32))
 
 
@@ -72,11 +83,11 @@ def main(read_delay: Union[int, str] = 60, **kwargs) -> None:
             pass
         else:
             print(f"{pm}")
+            last_msg_time = pm.time
         finally:
             delay = int(read_delay) - (time.time() - last_msg_time)
             if delay > 0:
                 time.sleep(delay)
-            last_msg_time = pm.time
 
 
 if __name__ == "__main__":
