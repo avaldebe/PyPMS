6a6b11899b7a435749989e2875fa8b02c85b267c
Alvaro Valdebenito <avaldebe@gmail.com>
2019-10-01 23:50:53 +0200

    split obsdata.py

--- pms/pm/obsdata/Plantower.py
+++ pms/pm/obsdata/Plantower.py
@@ -1,7 +1,7 @@
 """
-Extract measurements from
-- Plantower PMSx003 PM1/PM2.5/PM10 sensors
-- NovaFitness SDS01x PM2.5/PM10 sensors and SDS198 PM100 sensor
+Observations from Plantower messages
+- PMSx003 PM1/PM2.5/PM10 and 6 number concentrations
+- PMS3003 PM1/PM2.5/PM10
 """
 
 from datetime import datetime
@@ -92,90 +92,3 @@ class PMSx003(NamedTuple):
 
 class PMS3003(PMSx003):
     pass
-
-
-class SDS01x(NamedTuple):
-    """SDS01x observations
-    
-    time                                    measurement time [seconds since epoch]
-    raw25, raw10                            PM2.5*10, PM10*10 [ug/m3]
-    """
-
-    # seconds since epoch
-    time: int
-    # rawX [0.1 ug/m3]: PM2.5*10 & PM10*10
-    raw25: int
-    raw10: int
-
-    def subset(self, spec: str) -> Dict[str, float]:
-        if spec == "pm":
-            return {"pm25": self.raw25 / 10, "pm10": self.raw10 / 10}
-        raise ValueError(
-            f"Unknown subset code '{spec}' "
-            f"for object of type '{__name__}.{self.__class__.__name__}'"
-        )
-
-    @property
-    def date(self) -> datetime:
-        """measurement time as datetime object"""
-        return datetime.fromtimestamp(self.time)
-
-    def __format__(self, spec: str) -> str:
-        d = f = None
-        if spec.endswith("pm"):
-            d = (spec[:-2] or ".1") + "f"
-            return f"{self.date:%F %T}: PM2.5 {{pm25:{d}}}, PM10 {{pm10:{d}}} ug/m3".format_map(
-                self.subset("pm")
-            )
-        if spec.endswith("csv"):
-            d = (spec[:-3] or ".1") + "f"
-            return f"{self.time}, {{pm25:{d}}}, {{pm10:{d}}}".format_map(self.subset("pm"))
-        raise ValueError(
-            f"Unknown format code '{spec}' "
-            f"for object of type '{__name__}.{self.__class__.__name__}'"
-        )
-
-    def __str__(self):
-        return self.__format__("pm")
-
-
-class SDS198(NamedTuple):
-    """SDS198 observations
-    
-    time                                    measurement time [seconds since epoch]
-    pm100                                   PM100 [ug/m3]
-    """
-
-    # seconds since epoch
-    time: int
-    # rawX [ug/m3]: PM100
-    pm100: int
-
-    def subset(self, spec: str) -> Dict[str, float]:
-        if spec == "pm":
-            return {"pm100": self.pm100}
-        raise ValueError(
-            f"Unknown subset code '{spec}' "
-            f"for object of type '{__name__}.{self.__class__.__name__}'"
-        )
-
-    @property
-    def date(self) -> datetime:
-        """measurement time as datetime object"""
-        return datetime.fromtimestamp(self.time)
-
-    def __format__(self, spec: str) -> str:
-        d = f = None
-        if spec.endswith("pm"):
-            d = (spec[:-2] or ".1") + "f"
-            return f"{self.date:%F %T}: PM100 {self.pm100:{d}}"
-        if spec.endswith("csv"):
-            d = (spec[:-3] or ".1") + "f"
-            return f"{self.time}, {self.pm100:{d}}"
-        raise ValueError(
-            f"Unknown format code '{spec}' "
-            f"for object of type '{__name__}.{self.__class__.__name__}'"
-        )
-
-    def __str__(self):
-        return self.__format__("pm")
