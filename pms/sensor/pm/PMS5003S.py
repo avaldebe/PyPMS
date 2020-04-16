019e46606fd341c5d9253dda6505124e56371123
Alvaro Valdebenito <alvarov@met.no>
2020-04-16 12:44:04 +0200

    fix HCHO unit, close #9

--- pms/sensor/pm/PMS5003S.py
+++ pms/sensor/pm/PMS5003S.py
@@ -25,7 +25,7 @@ class ObsData(PMSx003.ObsData):
     raw01, raw25, raw10                     cf=1 PM estimates [ug/m3]
     pm01, pm25, pm10                        PM1.0, PM2.5, PM10 [ug/m3]
     n0_3, n0_5, n1_0, n2_5, n5_0, n10_0     number concentrations under X.Y um [#/cm3]
-    HCHO                                    formaldehyde concentration [ug/m3]
+    HCHO                                    formaldehyde concentration [mg/m3]
     """
 
     # datasheet says [1/1000 mg/m3], which is [ug/m3]
@@ -38,7 +38,7 @@ class ObsData(PMSx003.ObsData):
             csv = super().__format__(spec)
             return f"{csv}, {self.HCHO}"
         if spec == "hcho":
-            return f"{self.date:%F %T}: HCHO {self.HCHO} ug/m3"
+            return f"{self.date:%F %T}: HCHO {self.HCHO} mg/m3"
         raise ValueError(
             f"Unknown format code '{spec}' "
             f"for object of type '{__name__}.{self.__class__.__name__}'"
