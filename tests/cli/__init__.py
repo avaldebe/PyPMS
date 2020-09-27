7ff22290a08cddb02194894a376f0931ae41c8d3
Alvaro Valdebenito <alvarov@met.no>
2020-09-27 20:15:39 +0200

    use hexdump from docs as captured data

--- tests/cli/__init__.py
+++ tests/cli/__init__.py
@@ -1,6 +1,7 @@
+from csv import DictReader
 from enum import Enum
-from datetime import datetime
-from typing import NamedTuple, Dict, List, Generator, Optional
+from pathlib import Path
+from typing import NamedTuple, Dict, List, Tuple, Generator, Optional
 
 from pms.sensor import Sensor, base
 
@@ -22,35 +23,25 @@ class Obs(NamedTuple):
     def decode(self, sensor: Sensor) -> base.ObsData:
         return sensor.decode(self.raw, time=self.time)
 
+    @classmethod
+    def from_csv(cls, path: Path) -> Tuple["Obs", ...]:
+        with path.open() as csv:
+            reader = DictReader(csv, delimiter=";")
+            return tuple(cls(**row) for row in reader)
+
 
 class CapturedData(Enum):
+    """Captured data from /docs/sensors"""
 
-    PMSx003 = (  # pms -m PMSx003 -n 5 -i 10 serial -f csv
-        Obs(
-            "424d001c00000008000b00000008000b00b40039002e001e00000000970002a1",
-            "1601120880, 0, 8, 11, 0.0, 8.0, 11.0, 1.80, 0.57, 0.46, 0.30, 0.00, 0.00",
-        ),
-        Obs(
-            "424d001c000000080008000000080008011d005c001e001e0000000097000218",
-            "1601120890, 0, 8, 8, 0.0, 8.0, 8.0, 2.85, 0.92, 0.30, 0.30, 0.00, 0.00",
-        ),
-        Obs(
-            "424d001c0000000500050000000500050024000c000c000c000000009700019e",
-            "1601120900, 0, 5, 5, 0.0, 5.0, 5.0, 0.36, 0.12, 0.12, 0.12, 0.00, 0.00",
-        ),
-        Obs(
-            "424d001c000000020004000000020004001e000a000300030003000397000182",
-            "1601120910, 0, 2, 4, 0.0, 2.0, 4.0, 0.30, 0.10, 0.03, 0.03, 0.03, 0.03",
-        ),
-        Obs(
-            "424d001c00000000000100000000000100120006000300030003000397000168",
-            "1601120920, 0, 0, 1, 0.0, 0.0, 1.0, 0.18, 0.06, 0.03, 0.03, 0.03, 0.03",
-        ),
-    )
+    PMS3003 = Obs.from_csv(Path("tests/cli/captured_data/PMS3003.csv"))
+    PMSx003 = Obs.from_csv(Path("tests/cli/captured_data/PMSx003.csv"))
+    SDS01x = Obs.from_csv(Path("tests/cli/captured_data/SDS01x.csv"))
+    SDS198 = Obs.from_csv(Path("tests/cli/captured_data/SDS198.csv"))
+    MCU680 = Obs.from_csv(Path("tests/cli/captured_data/MCU680.csv"))
 
     @property
     def samples(self) -> int:
-        return len(self.value)
+        return len(self.value) - 2
 
     @property
     def interval(self) -> int:
@@ -68,10 +59,8 @@ class CapturedData(Enum):
 
     @property
     def csv(self) -> str:
-        heder = dict(
-            PMSx003="time, raw01, raw25, raw10, pm01, pm25, pm10, n0_3, n0_5, n1_0, n2_5, n5_0, n10_0",
-        )[self.name]
-        csv = "\n".join(obs.csv for obs in self.value)
+        heder = self.value[1].csv
+        csv = "\n".join(obs.csv for obs in self.value[2:])
         return f"{heder}\n{csv}\n"
 
 
@@ -101,5 +90,5 @@ class MockReader:
         pass
 
     def __call__(self) -> Generator[base.ObsData, None, None]:
-        for obs in self.data.value:
+        for obs in self.data.value[2:]:
             yield obs.decode(self.sensor)
