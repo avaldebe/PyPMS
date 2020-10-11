50b7c740db81934adb93a4757ae1cede6b4d0b56
Alvaro Valdebenito <alvarov@met.no>
2020-10-11 21:46:45 +0200

    cleaunu string format test

--- tests/sensor/test_obsdata.py
+++ tests/sensor/test_obsdata.py
@@ -17,29 +17,21 @@ def test_PMSx003_format(fmt, raw=tuple(range(1, 13)), secs=1_567_198_523, sensor
     obs = sensor.ObsData(secs, *raw)
     raw = raw[:6] + tuple(x / 100 for x in raw[6:])
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv="{}, {:d}, {:d}, {:d}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(
-            secs, *raw
-        ),
-        pm="{}: PM1 {:.1f}, PM2.5 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[3:6]
-        ),
-        num="{}: N0.3 {:.2f}, N0.5 {:.2f}, N1.0 {:.2f}, N2.5 {:.2f}, N5.0 {:.2f}, N10 {:.2f} #/cm3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[6:]
-        ),
-        cf="{}: CF1 {:.0%}, CF2.5 {:.0%}, CF10 {:.0%}".format(
-            time.strftime("%F %T", time.localtime(secs)),
-            raw[3] / raw[0],
-            raw[4] / raw[1],
-            raw[5] / raw[2],
-        ),
-        raw="{}: PM1 {:d}, PM2.5 {:d}, PM10 {:d} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[:3]
-        ),
+        raw="{0}: PM1 {1:d}, PM2.5 {2:d}, PM10 {3:d} μg/m3",
+        pm="{0}: PM1 {4:.1f}, PM2.5 {5:.1f}, PM10 {6:.1f} μg/m3",
+        num="{0}: N0.3 {7:.2f}, N0.5 {8:.2f}, N1.0 {9:.2f}, N2.5 {10:.2f}, N5.0 {11:.2f}, N10 {12:.2f} #/cm3",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:d}, {:d}, {:d}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
+    elif fmt == "cf":
+        cf = "{}: CF1 {:.0%}, CF2.5 {:.0%}, CF10 {:.0%}"
+        assert f"{obs:{fmt}}" == cf.format(date, raw[3] / raw[0], raw[4] / raw[1], raw[5] / raw[2])
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -54,33 +46,23 @@ def test_PMS5003ST_format(fmt, raw=list(range(1, 16)), secs=1_567_198_523, senso
     raw[13] /= 10
     raw[14] /= 10
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv="{}, {:d}, {:d}, {:d}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.3f}, {:.1f}, {:.1f}".format(
-            secs, *raw
-        ),
-        pm="{}: PM1 {:.1f}, PM2.5 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[3:6]
-        ),
-        num="{}: N0.3 {:.2f}, N0.5 {:.2f}, N1.0 {:.2f}, N2.5 {:.2f}, N5.0 {:.2f}, N10 {:.2f} #/cm3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[6:-3]
-        ),
-        cf="{}: CF1 {:.0%}, CF2.5 {:.0%}, CF10 {:.0%}".format(
-            time.strftime("%F %T", time.localtime(secs)),
-            raw[3] / raw[0],
-            raw[4] / raw[1],
-            raw[5] / raw[2],
-        ),
-        raw="{}: PM1 {:d}, PM2.5 {:d}, PM10 {:d} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[:3]
-        ),
-        hcho="{}: HCHO {:.3f} mg/m3".format(time.strftime("%F %T", time.localtime(secs)), raw[-3]),
-        atm="{}: Temp. {:.1f} °C, Rel.Hum. {:.1f} %".format(
-            time.strftime("%F %T", time.localtime(secs)), raw[-2], raw[-1]
-        ),
+        raw="{0}: PM1 {1:d}, PM2.5 {2:d}, PM10 {3:d} μg/m3",
+        pm="{0}: PM1 {4:.1f}, PM2.5 {5:.1f}, PM10 {6:.1f} μg/m3",
+        num="{0}: N0.3 {7:.2f}, N0.5 {8:.2f}, N1.0 {9:.2f}, N2.5 {10:.2f}, N5.0 {11:.2f}, N10 {12:.2f} #/cm3",
+        hcho="{0}: HCHO {13:.3f} mg/m3",
+        atm="{0}: Temp. {14:.1f} °C, Rel.Hum. {15:.1f} %",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:d}, {:d}, {:d}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.3f}, {:.1f}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
+    elif fmt == "cf":
+        cf = "{}: CF1 {:.0%}, CF2.5 {:.0%}, CF10 {:.0%}"
+        assert f"{obs:{fmt}}" == cf.format(date, raw[3] / raw[0], raw[4] / raw[1], raw[5] / raw[2])
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -92,32 +74,22 @@ def test_PMS5003T_format(fmt, raw=tuple(range(1, 13)), secs=1_567_198_523, senso
     obs = sensor.ObsData(secs, *raw)
     raw = raw[:6] + tuple(x / 100 for x in raw[6:-2]) + tuple(x / 10 for x in raw[-2:])
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv="{}, {:d}, {:d}, {:d}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.1f}, {:.1f}".format(
-            secs, *raw
-        ),
-        pm="{}: PM1 {:.1f}, PM2.5 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[3:6]
-        ),
-        num="{}: N0.3 {:.2f}, N0.5 {:.2f}, N1.0 {:.2f}, N2.5 {:.2f} #/cm3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[6:-2]
-        ),
-        cf="{}: CF1 {:.0%}, CF2.5 {:.0%}, CF10 {:.0%}".format(
-            time.strftime("%F %T", time.localtime(secs)),
-            raw[3] / raw[0],
-            raw[4] / raw[1],
-            raw[5] / raw[2],
-        ),
-        raw="{}: PM1 {:d}, PM2.5 {:d}, PM10 {:d} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[:3]
-        ),
-        atm="{}: Temp. {:.1f} °C, Rel.Hum. {:.1f} %".format(
-            time.strftime("%F %T", time.localtime(secs)), raw[-2], raw[-1]
-        ),
+        raw="{0}: PM1 {1:d}, PM2.5 {2:d}, PM10 {3:d} μg/m3",
+        pm="{0}: PM1 {4:.1f}, PM2.5 {5:.1f}, PM10 {6:.1f} μg/m3",
+        num="{0}: N0.3 {7:.2f}, N0.5 {8:.2f}, N1.0 {9:.2f}, N2.5 {10:.2f} #/cm3",
+        atm="{0}: Temp. {11:.1f} °C, Rel.Hum. {12:.1f} %",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:d}, {:d}, {:d}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.1f}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
+    elif fmt == "cf":
+        cf = "{}: CF1 {:.0%}, CF2.5 {:.0%}, CF10 {:.0%}"
+        assert f"{obs:{fmt}}" == cf.format(date, raw[3] / raw[0], raw[4] / raw[1], raw[5] / raw[2])
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -126,19 +98,19 @@ def test_PMS5003T_format(fmt, raw=tuple(range(1, 13)), secs=1_567_198_523, senso
 
 @pytest.mark.parametrize("fmt", "header csv pm error".split())
 def test_SDS01x_format(fmt, raw=(11, 12), secs=1_567_198_523, sensor=sds01x):
-
     obs = sensor.ObsData(secs, *raw)
     raw = tuple(r / 10 for r in raw)
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv=f"{secs}, " + ", ".join(map("{:.1f}".format, raw)),
-        pm="{}: PM2.5 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw
-        ),
+        pm="{}: PM2.5 {:.1f}, PM10 {:.1f} μg/m3",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:.1f}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -147,16 +119,18 @@ def test_SDS01x_format(fmt, raw=(11, 12), secs=1_567_198_523, sensor=sds01x):
 
 @pytest.mark.parametrize("fmt", "header csv pm error".split())
 def test_SDS198_format(fmt, raw=123, secs=1_567_198_523, sensor=sds198):
-
     obs = sensor.ObsData(secs, raw)
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv=f"{secs}, {raw:.1f}",
-        pm="{}: PM100 {:.1f} μg/m3".format(time.strftime("%F %T", time.localtime(secs)), raw),
+        pm="{}: PM100 {:.1f} μg/m3",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, raw)
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -167,15 +141,16 @@ def test_SDS198_format(fmt, raw=123, secs=1_567_198_523, sensor=sds198):
 def test_HPMA115S0_format(fmt, raw=(11, 12), secs=1_567_198_523, sensor=hpma115s0):
     obs = sensor.ObsData(secs, *raw)
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv=f"{secs}, " + ", ".join(map("{:.1f}".format, raw)),
-        pm="{}: PM2.5 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw
-        ),
+        pm="{}: PM2.5 {:.1f}, PM10 {:.1f} μg/m3",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:.1f}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -186,15 +161,16 @@ def test_HPMA115S0_format(fmt, raw=(11, 12), secs=1_567_198_523, sensor=hpma115s
 def test_HPMA115C0_format(fmt, raw=(11, 12, 13, 14), secs=1_567_198_523, sensor=hpma115c0):
     obs = sensor.ObsData(secs, *raw)
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv=f"{secs}, " + ", ".join(map("{:.1f}".format, raw)),
-        pm="{}: PM1 {:.1f}, PM2.5 {:.1f}, PM4 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw
-        ),
+        pm="{}: PM1 {:.1f}, PM2.5 {:.1f}, PM4 {:.1f}, PM10 {:.1f} μg/m3",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:.1f}, {:.1f}, {:.1f}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -203,24 +179,20 @@ def test_HPMA115C0_format(fmt, raw=(11, 12, 13, 14), secs=1_567_198_523, sensor=
 
 @pytest.mark.parametrize("fmt", "header csv pm num diam error".split())
 def test_SPS30_format(fmt, raw=range(100, 110), secs=1_567_198_523, sensor=sps30):
-
     obs = sensor.ObsData(secs, *raw)
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv="{}, {:.1f}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.1f}".format(
-            secs, *raw
-        ),
-        pm="{}: PM1 {:.1f}, PM2.5 {:.1f}, PM4 {:.1f}, PM10 {:.1f} μg/m3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[:4]
-        ),
-        num="{}: N0.5 {:.2f}, N1.0 {:.2f}, N2.5 {:.2f}, N4.0 {:.2f}, N10 {:.2f} #/cm3".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[4:-1]
-        ),
-        diam="{}: Ø {:.1f} μm".format(time.strftime("%F %T", time.localtime(secs)), raw[-1]),
+        pm="{0}: PM1 {1:.1f}, PM2.5 {2:.1f}, PM4 {3:.1f}, PM10 {4:.1f} μg/m3",
+        num="{0}: N0.5 {5:.2f}, N1.0 {6:.2f}, N2.5 {7:.2f}, N4.0 {8:.2f}, N10 {9:.2f} #/cm3",
+        diam="{0}: Ø {10:.1f} μm",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:.1f}, {:.1f}, {:.1f}, {:.1f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.1f}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
@@ -229,7 +201,6 @@ def test_SPS30_format(fmt, raw=range(100, 110), secs=1_567_198_523, sensor=sps30
 
 @pytest.mark.parametrize("fmt", "header csv atm bme bsec error".split())
 def test_mcu680_format(fmt, raw=list(range(100, 107)), secs=1_567_198_523, sensor=mcu680):
-
     obs = sensor.ObsData(secs, *raw)
     raw[0] /= 100
     raw[1] /= 100
@@ -238,21 +209,18 @@ def test_mcu680_format(fmt, raw=list(range(100, 107)), secs=1_567_198_523, senso
     raw[4] &= 0x0FFF
     raw[5] /= 1000
     obs_fmt = dict(
-        header=", ".join(asdict(obs).keys()),
-        csv="{}, {:.1f}, {:.1f}, {:.2f}, {:}, {:}, {:.1f}, {:}".format(secs, *raw),
-        atm="{}: Temp. {:.1f} °C, Rel.Hum. {:.1f} %, Press {:.2f} hPa".format(
-            time.strftime("%F %T", time.localtime(secs)), *raw[:3]
-        ),
-        bme="{}: Temp. {:.1f} °C, Rel.Hum. {:.1f} %, Press {:.2f} hPa, {:.1f} kΩ".format(
-            time.strftime("%F %T", time.localtime(secs)), raw[0], raw[1], raw[2], raw[5]
-        ),
-        bsec="{}: Temp. {:.1f} °C, Rel.Hum. {:.1f} %, Press {:.2f} hPa, {} IAQ".format(
-            time.strftime("%F %T", time.localtime(secs)), raw[0], raw[1], raw[2], raw[4]
-        ),
+        atm="{0}: Temp. {1:.1f} °C, Rel.Hum. {2:.1f} %, Press {3:.2f} hPa",
+        bsec="{0}: Temp. {1:.1f} °C, Rel.Hum. {2:.1f} %, Press {3:.2f} hPa, {5} IAQ",
+        bme="{0}: Temp. {1:.1f} °C, Rel.Hum. {2:.1f} %, Press {3:.2f} hPa, {6:.1f} kΩ",
     )
-
+    date = time.strftime("%F %T", time.localtime(secs))
     if fmt in obs_fmt:
-        assert f"{obs:{fmt}}" == obs_fmt[fmt]
+        assert f"{obs:{fmt}}" == obs_fmt[fmt].format(date, *raw)
+    elif fmt == "header":
+        assert f"{obs:{fmt}}" == ", ".join(asdict(obs).keys())
+    elif fmt == "csv":
+        csv = "{}, {:.1f}, {:.1f}, {:.2f}, {:}, {:}, {:.1f}, {:}"
+        assert f"{obs:{fmt}}" == csv.format(secs, *raw)
     else:
         with pytest.raises(ValueError) as e:
             f"{obs:{fmt}}"
