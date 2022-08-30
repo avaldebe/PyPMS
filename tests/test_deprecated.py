cb4bd6dd6796b0dcfb87977b0b5d786b4758a9b5
Alvaro Valdebenito <alvarov@met.no>
2022-08-30 11:24:41 +0200

    use poetry-dynamic-versioning

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -6,20 +6,21 @@ else:  # pragma: no cover
     import importlib_metadata as metadata
 
 import pytest
+from packaging.version import Version
 
 from .sensor.test_sensor import GoodData
 
-sem_ver: str = metadata.version("PyPMS")
-version = tuple(int(v) for v in sem_ver.split("."))
+version = Version(metadata.version("PyPMS"))
+pytestmark = pytest.mark.skipif(
+    version >= Version("1.0"), reason="deprecated module should be removed on 1.0 release"
+)
 
 
-@pytest.mark.skipif(version >= (1, 0), reason="deprecated module should be removed on 1.0 release")
 def test_deprecated_module():
     with pytest.deprecated_call():
         import pms.sensor
 
 
-@pytest.mark.skipif(version >= (1, 0), reason="deprecated module should be removed on 1.0 release")
 @pytest.mark.parametrize("obs", GoodData.test_obs())
 def test_deprecated_method(obs):
     with pytest.deprecated_call():
